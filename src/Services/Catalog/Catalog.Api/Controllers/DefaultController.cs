3d972df78ad061c16dede3a5e44006505fc4f9cb
deivisjl <deivisjl@gmail.com>
2020-07-26 16:46:57 -0600

    Creando la Api de catalogo

---
+++ src/Services/Catalog/Catalog.Api/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers
+{
+    [Route("/")]
+    public class DefaultController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Running...";
+        }
+    }
+}
