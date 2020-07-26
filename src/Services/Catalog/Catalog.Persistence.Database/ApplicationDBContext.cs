f769d67b2d4c4232609105eeb04bded51ed92cfa
deivisjl <deivisjl@gmail.com>
2020-07-26 17:14:36 -0600

    Creando la clase de contexto

---
+++ src/Services/Catalog/Catalog.Persistence.Database/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using CatalogDomain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Catalog.Persistence.Database
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(
+            DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductInStock> Stocks { get; set; }
+    }
+}
